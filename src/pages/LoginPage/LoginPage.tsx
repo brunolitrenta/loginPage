015feee33440c28d133c94ff31c8f33241cc206b
Bruno Litrenta <144285558+brunolitrenta@users.noreply.github.com>
2024-04-25 22:05:42 -0300

    Added show/hide password button

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import { FooterComponent } from '../../components/footerComponent/footerComponent'
 import lightImage from '../../assets/light-svgrepo-com.svg'
 import moonImage from '../../assets/moon-svgrepo-com.svg'
+import eyeBlack from '../../assets/eye-solid-black.svg'
+import eyeWhite from '../../assets/eye-solid-white.svg'
+import eyeSlashedBlack from '../../assets/eye-slash-solid-black.svg'
+import eyeSlashedWhite from '../../assets/eye-slash-solid-white.svg'
 import './LoginPage.css'
 
 export function LoginPage() {
 
+
+
   const [switchTheme, setSwitchTheme] = useState<string>('dark')
 
   const [themeImage, setThemeImage] = useState<string>(lightImage)
+
+  const [buttonPasswordImage, setButtonPasswordImage] = useState<string>(eyeWhite)
+
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   function changeTheme() {
     if (switchTheme != 'light') {
@@ -22,9 +32,33 @@ export function LoginPage() {
   function changeThemeButton() {
     if (themeImage == lightImage) {
       setThemeImage(moonImage)
+      setButtonPasswordImage(eyeBlack)
     }
     else {
       setThemeImage(lightImage)
+      setButtonPasswordImage(eyeWhite)
+    }
+  }
+
+  function makePasswordVisible(){
+    setShowPassword(!showPassword)
+  }
+
+  function changePassButtonImage(){
+    if(showPassword && switchTheme == 'dark'){
+      setButtonPasswordImage(eyeWhite)
+      return
+    }
+    else if(showPassword && switchTheme == 'light'){
+      setButtonPasswordImage(eyeBlack)
+      return
+    }
+    else if(!showPassword && switchTheme == 'dark'){
+      setButtonPasswordImage(eyeSlashedWhite)
+      return
+    }
+    else if(!showPassword && switchTheme == 'light'){
+      setButtonPasswordImage(eyeSlashedBlack)
     }
   }
 
@@ -36,14 +70,23 @@ export function LoginPage() {
         <section className='loginBlock'>
           <div className='userInput'>
             <label>Usuário ou E-mail</label>
-            <input type="email" />
+            <input className='input' type="email" />
           </div>
           <div className='keyInput'>
             <div className='senhaLabel'>
               <label>Senha</label>
               <a href="#######">Esqueceu a senha?</a>
             </div>
-            <input type="password" />
+            <div className='showSenha'>
+              {
+                !showPassword 
+                ?
+                <input className='input inputSenha' type="password" />
+                :
+                <input className='input inputSenha' type="text" />
+              }
+              <button className='showPassButton' onClick={() => {makePasswordVisible(); changePassButtonImage()}}><img src={buttonPasswordImage} alt="" /></button>
+            </div>
           </div>
           <button>Entrar</button>
         </section>
